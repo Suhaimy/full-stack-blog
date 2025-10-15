9ddd4ec92a98457fb911a9422280c4bbbed370f8
Suhaimy Samsudin <suhaimysamsudin@meta.com>
2025-10-15 17:15:35 +0800

    updated backend and included routes

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,9 +1,16 @@
 import express from "express";
+import {
+  getPosts,
+  getPost,
+  createPost,
+  deletePost,
+} from "../controllers/post.controller.js";
 
 const router = express.Router();
 
-router.get("/test", (req, res) => {
-  res.status(200).send("post route");
-});
+router.get("/", getPosts);
+router.get("/:slug", getPost);
+router.post("/", createPost);
+router.delete("/:id", deletePost);
 
 export default router;
